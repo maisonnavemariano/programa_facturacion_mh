21d94c3a9fe22280c0c0ce32ece3f1d6b76fd618
maiso <maiso@debian-pc>
2017-01-11 17:11:33 -0300

    ggg

--- MaisonnaveHermosilla-v0/src/controller/model/Cliente.java
+++ MaisonnaveHermosilla-v0/src/controller/model/Cliente.java
@@ -4,7 +4,8 @@ import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
-//comentario  
+//comentario  caca
+
 
 /**
  * Clase modelo para la entidad Cliente
