7eff2426036ba31c7f181e7b7b8250af55c2d76b
maiso <maiso@debian-pc>
2017-01-11 17:03:10 -0300

    Prueba de commit y push; agregue un comentario en la linea 7

--- MaisonnaveHermosilla-v0/src/controller/model/Cliente.java
+++ MaisonnaveHermosilla-v0/src/controller/model/Cliente.java
@@ -4,6 +4,7 @@ import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+//comentario
 
 /**
  * Clase modelo para la entidad Cliente
