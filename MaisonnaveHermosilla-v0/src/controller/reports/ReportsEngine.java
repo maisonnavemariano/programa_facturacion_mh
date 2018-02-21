17e84987c516b41102cc20726762f808c96aa04e
maiso <maisonnavemariano@gmail.com>
2018-02-21 10:20:18 -0300

    Ahora generarReporte es estático

--- MaisonnaveHermosilla-v0/src/controller/reports/ReportsEngine.java
+++ MaisonnaveHermosilla-v0/src/controller/reports/ReportsEngine.java
@@ -4,14 +4,14 @@ import controller.db.Presupuesto;
 
 public class ReportsEngine {
 	
-	public String generarReporte(Presupuesto p) {
+	public static String generarReporte(Presupuesto p) {
 		return "reportes/Original_y_copia.pdf";
 	}
-	public String generarReporteBorrador(Presupuesto p) {
+	public static String generarReporteBorrador(Presupuesto p) {
 		return "reporters/Borrador.pdf";
 	}
 	
-	public void generarReporteBorrador(Presupuesto p, String url) {
+	public static void generarReporteBorrador(Presupuesto p, String url) {
 		
 	}
 
