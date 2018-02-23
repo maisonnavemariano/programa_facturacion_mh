e027235a58ee9280908a023248f73ade9528ec05
maiso <maisonnavemariano@gmail.com>
2018-02-23 14:11:06 -0300

    Librerías de jasperreport agregadas

--- MaisonnaveHermosilla-v0/src/controller/reports/tester.java
+++ MaisonnaveHermosilla-v0/src/controller/reports/tester.java
@@ -7,8 +7,13 @@ public class tester {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		DBEngine motor = new DBEngine();
-		ReportsEngine.generarReporte(motor.verPresupuesto(2575));
-		ReportsEngine.generarReporteBorrador(motor.verPresupuesto(2575));
+		System.out.println("arranca reportes");
+		ReportsEngine.generarReporte(motor.verPresupuesto(2175),"/home/maiso/tmp.pdf");
+		System.out.println("reporte 1");
+		ReportsEngine.generarReporte(motor.verPresupuesto(2775));
+		System.out.println("reporte 2");
+		//ReportsEngine.generarReporteBorrador(motor.verPresupuesto(2575));
+		System.out.println("reporte 3");
 
 	}
 
