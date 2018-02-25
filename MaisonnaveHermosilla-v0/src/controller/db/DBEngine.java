803037e18e797cad53b4ae76bbc01b619e3f3937
maiso <maisonnavemariano@gmail.com>
2018-02-25 19:15:27 -0300

    Métodos corregidos y docs.
    metodos de aplicar aumento y deshacer corregido. Todo documentado menos
    el metodo de recalcular subtotales

--- MaisonnaveHermosilla-v0/src/controller/db/DBEngine.java
+++ MaisonnaveHermosilla-v0/src/controller/db/DBEngine.java
@@ -980,6 +980,7 @@ public class DBEngine {
 	}
 	
 	
+	
 	private void recalcularSubtotales() {
 		PreparedStatement preparedStmt;
 		String query = "SELECT p.Nro_presupuesto, ROUND(SUM(cp.Monto)*20)/20 AS Subtotal  "
